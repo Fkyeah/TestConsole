d5d1ab191c9eb6c0b1a0ae4555d371ca19e9c053
Fkyeah <dimx63@mail.ru>
2022-05-28 21:25:23 +0300

    test main project

---
+++ TestConsole.Main/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TestConsole.Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 1;
+
+            var math = new MathMethods();
+
+            var result = math.Plus(a, b);
+
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
